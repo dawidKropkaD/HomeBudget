7559be461541c7b3486b2a5a4bfba35f04b62c59
Dawid <dawid@example.com>
2018-11-17 20:34:58 +0100

    Begin implementing the addition of expenses

---
+++ HomeBudget/Controllers/ExpensesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeBudget.ViewModels.Expenses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBudget.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        public IActionResult Add()
+        {
+            var context = new DataAccess.Models.HomeBudgetContext();
+            AddViewModel vm = new AddViewModel(context.Units.ToList());
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddViewModel viewModel)
+        {
+            return View();
+        }
+
+        public JsonResult GetProductNames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HomeBudget/ViewModels/Expenses/AddViewModel.cs
@@ -0,0 +1,42 @@
+﻿using HomeBudget.DataAccess.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeBudget.ViewModels.Expenses
+{
+    public class AddViewModel
+    {
+        public AddViewModel()
+        {
+        }
+
+        public AddViewModel(List<Units> units)
+        {
+            UnitList = new SelectList(units, "Id", "Name");
+        }
+
+        [Display(Name = "Data")]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [Display(Name = "Produkt")]
+        [Required]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Cena")]
+        [Required]
+        [RegularExpression(@"^\d+.\d{0,2}$", ErrorMessage = "Cena nie może mieć więcej niż 2 miejsca po przecinku")]
+        public double? Price { get; set; }
+
+        [Display(Name = "Ilość")]
+        public int Quantity { get; set; } = 1;
+
+        [Display(Name = "Jednostka")]
+        public int SelectedUnitId { get; set; }
+
+        public SelectList UnitList { get; set; }
+    }
+}
